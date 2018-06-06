01c4f447838008a4c32de44ea7e5ea11c0af930e
kirillstarlight <kirillstarlight@gmail.com>
2018-06-06 09:29:35 +0300

    init commit

---
+++ mountCDWin.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo mkdir -p /media/kirillstarlight/383439CA34398BBE
+sudo mkdir -p /media/kirillstarlight/AA26A73E26A70B01
+sudo mkdir -p /media/kirillstarlight/Новый\ том/
+sudo mount -t ntfs -o nls=utf8,umask=0222 /dev/sda3 /media/kirillstarlight/AA26A73E26A70B01/
+sudo mount -t ntfs -o nls=utf8,umask=0222 /dev/sda6 /media/kirillstarlight/383439CA34398BBE/
+sudo mount -t ntfs -o nls=utf8,umask=0222 /dev/sda4 /media/kirillstarlight/Новый\ том/
